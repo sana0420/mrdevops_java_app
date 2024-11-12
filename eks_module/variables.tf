6ddabc0c6b97f5c5a3886336329e6e5b00fd2cdc
sana arshad <115351368+sana0420@users.noreply.github.com>
2024-11-12 23:52:16 +0500

    Update variables.tf

--- eks_module/variables.tf
+++ eks_module/variables.tf
@@ -1,14 +1,11 @@
 
 variable "access_key" {
-    default = ""
 }
 variable "secret_key" {
-    default = ""
 }
 
 
 variable "region" {
-    default = "us-east-1"
 }
 
 variable "aws_eks_cluster_config" {
