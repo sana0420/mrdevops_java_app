bfb417fa1e4d91bb8ccd9253db2d37da4c14560c
sana arshad <115351368+sana0420@users.noreply.github.com>
2024-11-15 03:12:42 +0500

    Update main.tf

--- eks_module/modules/aws_eks/main.tf
+++ eks_module/modules/aws_eks/main.tf
@@ -52,7 +52,9 @@ resource "aws_iam_role" "eks_cluster" {
         "Service": "eks.amazonaws.com"
       },
       "Action": "sts:AssumeRole"
-    },
+    }
+  ]
+},
 {
       "Effect": "Allow",
       "Action": "eks:CreateCluster",
